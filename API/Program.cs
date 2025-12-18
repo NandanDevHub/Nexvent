8ce4417502f0a8f5612e85fc3a58bfaa81dd38e1
nandan <parmar.nandan189@gmail.com>
2025-12-18 07:19:02 +0530

    Implement database migration and seeding logic in Program.cs; add DbInitializer for initial data population

--- API/Program.cs
+++ API/Program.cs
@@ -17,4 +17,18 @@ var app = builder.Build();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitalizer.SeedData(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An Error occoured during Migration.");
+}
+
 app.Run();
